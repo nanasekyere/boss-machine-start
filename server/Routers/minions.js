529475b470fd8a963134958603bd2f456f5c7a78
nanasekyere <aaronny13@icloud.com>
2026-01-09 17:10:55 +0000

    Prettier reformat

--- server/Routers/minions.js
+++ server/Routers/minions.js
@@ -6,72 +6,76 @@
     PUT /api/minions/:minionId to update a single minion by id.
     DELETE /api/minions/:minionId to delete a single minion by id. 
 */
-const {getFromDatabaseById, getAllFromDatabase, addToDatabase, updateInstanceInDatabase, deleteFromDatabasebyId} = require('../db')
-const express = require('express');
+const {
+  getFromDatabaseById,
+  getAllFromDatabase,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId,
+} = require("../db");
+const express = require("express");
 const router = express.Router();
 
-router.param('minionId', (req, res, next, id) => {
-    const minion = getFromDatabaseById('minions', id)
-    if (minion) {
-        req.minion = minion
-        next()
-    } else {
-        const err = new Error("Could not find minion with id: " + id )
-        err.status = 404
-        next(err)
-    }
-})
+router.param("minionId", (req, res, next, id) => {
+  const minion = getFromDatabaseById("minions", id);
+  if (minion) {
+    req.minion = minion;
+    next();
+  } else {
+    const err = new Error("Could not find minion with id: " + id);
+    err.status = 404;
+    next(err);
+  }
+});
 
-router.get('/', (req, res, next) => {
-    const minions = getAllFromDatabase('minions')
+router.get("/", (req, res, next) => {
+  const minions = getAllFromDatabase("minions");
 
-    if (!minions) {
-        const err = new Error("Could not find minion data on server" )
-        err.status = 404
-        next(err)
-    } else {
-        res.send(minions);
-    }
-})
+  if (!minions) {
+    const err = new Error("Could not find minion data on server");
+    err.status = 404;
+    next(err);
+  } else {
+    res.send(minions);
+  }
+});
 
-router.get('/:minionId', (req, res, next) => {
-    res.send(req.minion)
-})
+router.get("/:minionId", (req, res, next) => {
+  res.send(req.minion);
+});
 
-router.put('/:minionId', (req, res, next) => {
-    const minion = req.body
-    try {
-        res.send(updateInstanceInDatabase('minions', minion))
-    } catch (error) {
-        error.status = 422
-        next(error)
-    }
-})
+router.put("/:minionId", (req, res, next) => {
+  const minion = req.body;
+  try {
+    res.send(updateInstanceInDatabase("minions", minion));
+  } catch (error) {
+    error.status = 422;
+    next(error);
+  }
+});
 
-router.delete('/:minionId', (req, res, next) => {
-    const minion = req.minion
-    const deletion = deleteFromDatabasebyId('minions', minion.id)
+router.delete("/:minionId", (req, res, next) => {
+  const minion = req.minion;
+  const deletion = deleteFromDatabasebyId("minions", minion.id);
 
-    if (!deletion) {
-        const err = new Error("Could not find minion with id: " + minion.id )
-        err.status = 404
-        next(err)
-    } else {
-        res.status(200).send()
-    }
-})
+  if (!deletion) {
+    const err = new Error("Could not find minion with id: " + minion.id);
+    err.status = 404;
+    next(err);
+  } else {
+    res.status(200).send();
+  }
+});
 
+router.post("/", (req, res, next) => {
+  const newMinion = req.body;
 
-
-router.post('/', (req, res, next) => {
-    const newMinion = req.body
-
-    try {
-        res.send(addToDatabase('minions', newMinion))
-    } catch (error) {
-        error.status(400)
-        next(error)
-    }
-})
+  try {
+    res.send(addToDatabase("minions", newMinion));
+  } catch (error) {
+    error.status(400);
+    next(error);
+  }
+});
 
 module.exports = router;
